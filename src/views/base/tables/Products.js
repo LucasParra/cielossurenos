3e71542fff009e9199dd7a04e8cb1c6f417bfb3a
lparra <87325274+dseba@users.noreply.github.com>
2021-08-04 19:50:24 -0400

    table product

---
+++ src/views/base/tables/Products.js
@@ -0,0 +1,264 @@
+import React, { useEffect, useState } from "react";
+import {
+  CBadge,
+  CButton,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CDataTable,
+  CFormGroup,
+  CInput,
+  CLabel,
+  CModal,
+  CModalBody,
+  CModalFooter,
+  CModalHeader,
+  CModalTitle,
+  CRow,
+} from "@coreui/react";
+import { DocsLink } from "src/reusable";
+
+// import users from "../../users/users";
+import { supabase } from "src/config/configSupabase";
+import CIcon from "@coreui/icons-react";
+
+const getBadge = (status) => {
+  switch (status) {
+    case "Active":
+      return "success";
+    case "Inactive":
+      return "secondary";
+    case "Pending":
+      return "warning";
+    case "Banned":
+      return "danger";
+    default:
+      return "primary";
+  }
+};
+const fields = ["ID", "Name", "BasePrice", "show_details"];
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [product, setProduct] = useState({
+    Name: "",
+    BasePrice: 0,
+  });
+  const [loading, setLoading] = useState(false);
+  const componentDidMount = (limit = 1) => {
+    setLoading(true);
+    supabase
+      .from("Product")
+      .select("*")
+      .limit(limit * 5 + 1)
+      .then((snapshot) => {
+        setProducts(snapshot.data);
+        setLoading(false);
+      })
+      .catch(console.error);
+  };
+
+  const createProduct = () => {
+    if (product.ID)
+      return supabase
+        .from("Product")
+        .upsert(product)
+        .then(() => {
+          setLoading(false);
+          componentDidMount();
+          setModalVisible(false);
+        });
+
+    setLoading(true);
+    return supabase
+      .from("Product")
+      .insert(product)
+      .then(() => {
+        setLoading(false);
+        componentDidMount();
+        setModalVisible(false);
+      });
+  };
+  const deleteProduct = () =>
+    supabase
+      .from("Product")
+      .delete()
+      .match({ ID: product.ID })
+      .then(() => {
+        setLoading(false);
+        componentDidMount();
+        setDeleteModal(false);
+      });
+
+  useEffect(componentDidMount, []);
+  return (
+    <>
+      <CRow className="align-items-center">
+        <CCol col="2" xs="2" sm="2" md="2" className="mb-3 mb-xl-0">
+          <CButton block color="primary" onClick={() => setModalVisible(true)}>
+            Crear Productos
+          </CButton>
+        </CCol>
+      </CRow>
+
+      <CModal
+        show={modalVisible}
+        onClose={() => setModalVisible(!modalVisible)}
+        size="sm"
+      >
+        <CModalHeader closeButton>
+          <CModalTitle>Crear Productos</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          <CRow>
+            <CCol xs="12">
+              <CFormGroup>
+                <CLabel htmlFor="product">Nombre</CLabel>
+                <CInput
+                  id="product"
+                  placeholder="ingresa el nombre del producto"
+                  value={product.Name}
+                  onChange={({ target: { value } }) =>
+                    setProduct({ ...product, Name: value })
+                  }
+                  required
+                />
+              </CFormGroup>
+            </CCol>
+          </CRow>
+          <CRow>
+            <CCol xs="12">
+              <CFormGroup>
+                <CLabel htmlFor="priceBase">Precio Base</CLabel>
+                <CInput
+                  id="priceBase"
+                  value={product.BasePrice}
+                  onChange={({ target: { value } }) =>
+                    setProduct({ ...product, BasePrice: value })
+                  }
+                  placeholder="$50.000"
+                  required
+                />
+              </CFormGroup>
+            </CCol>
+          </CRow>
+        </CModalBody>
+        <CModalFooter>
+          <CButton color="primary" onClick={createProduct}>
+            Crear
+          </CButton>{" "}
+          <CButton
+            color="secondary"
+            onClick={() => setModalVisible(!modalVisible)}
+          >
+            Cancelar
+          </CButton>
+        </CModalFooter>
+      </CModal>
+      <CModal
+        show={deleteModal}
+        color="danger"
+        onClose={() => setDeleteModal(!deleteModal)}
+        size="sm"
+      >
+        <CModalHeader closeButton>
+          <CModalTitle>Eliminar Producto</CModalTitle>
+        </CModalHeader>
+        <CModalFooter>
+          <CButton color="danger" onClick={deleteProduct}>
+            Si
+          </CButton>
+          <CButton
+            onClick={() => setDeleteModal(!deleteModal)}
+            color="secondary"
+          >
+            No
+          </CButton>
+        </CModalFooter>
+      </CModal>
+
+      <CRow>
+        <CCol xs="12" lg="12">
+          <CCard>
+            <CCardHeader>
+              Productos
+              <DocsLink name="CModal" />
+            </CCardHeader>
+            <CCardBody>
+              <CDataTable
+                items={products}
+                fields={fields}
+                itemsPerPage={5}
+                onPageChange={componentDidMount}
+                loading={loading}
+                pagination
+                scopedSlots={{
+                  status: (item) => (
+                    <td>
+                      <CBadge color={getBadge(item.status)}>
+                        {item.status}
+                      </CBadge>
+                    </td>
+                  ),
+                  show_details: (item, index) => (
+                    <td className="py-2">
+                      <CRow className="align-items-center">
+                        <CCol
+                          col="2"
+                          xs="2"
+                          sm="2"
+                          md="2"
+                          className="mb-2 mb-xl-0"
+                        >
+                          <CButton
+                            color="primary"
+                            onClick={() => {
+                              setProduct(item);
+                              setModalVisible(true);
+                            }}
+                          >
+                            <CIcon
+                              name="cil-pencil"
+                              style={{ paddingLeft: 10 }}
+                              customClasses="c-sidebar-nav-icon"
+                            />
+                          </CButton>
+                        </CCol>
+                        <CCol
+                          col="2"
+                          xs="2"
+                          sm="2"
+                          md="2"
+                          className="mb-2 mb-xl-0"
+                        >
+                          <CButton
+                            color="danger"
+                            onClick={() => {
+                              setProduct(item);
+                              setDeleteModal(true);
+                            }}
+                          >
+                            <CIcon
+                              name="cil-trash"
+                              style={{ paddingLeft: 10 }}
+                              customClasses="c-sidebar-nav-icon"
+                            />
+                          </CButton>
+                        </CCol>
+                      </CRow>
+                    </td>
+                  ),
+                }}
+              />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </>
+  );
+};
+
+export default Products;
